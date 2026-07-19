7d17f0856c7205d79bd9ab1da0679b450439d21b
kundangitwala <github23scse1011592@gmail.com>
2026-07-19 10:37:21 +0530

    Time: 0 ms (100%), Space: 9.9 MB (13.03%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+         int n=s.length();
+        stack<char> st;
+        unordered_map<char,int> lastIndex;
+        unordered_map<char,bool> seen;
+        for(int i=0;i<n; i++)
+        {
+            lastIndex[s[i]] = i;
+        }
+        for(int i=0;i<n; i++)
+        {
+            if(seen[s[i]]) continue;
+            while(!st.empty() && st.top() > s[i] && lastIndex[st.top()] > i)
+            {
+                seen[st.top()]=false;
+                st.pop();
+                
+            }
+            st.push(s[i]);
+            seen[s[i]] = true;
+        }
+        string ans="";
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
