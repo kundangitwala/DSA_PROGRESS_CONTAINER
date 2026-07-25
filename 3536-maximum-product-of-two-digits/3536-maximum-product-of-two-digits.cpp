edd87d9bd930d19a33261074fb1a6c587513c6b6
kundangitwala <github23scse1011592@gmail.com>
2026-07-25 00:13:35 -0700

    Time: 0 ms (100%), Space: 8.7 MB (74.03%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -6,7 +6,7 @@ public:
         while(n!=0)
         {
             int digit=n%10;
-            if(digit>first)
+            if(digit>=first)
             {
                 second=first;
                 first=digit;
