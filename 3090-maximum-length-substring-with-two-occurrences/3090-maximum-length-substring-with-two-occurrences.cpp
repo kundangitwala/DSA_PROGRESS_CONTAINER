33c16500081c993c673e61b93ff6dccdf929b896
kundangitwala <github23scse1011592@gmail.com>
2026-08-13 22:26:59 -0700

    Time: 3 ms (42.93%), Space: 9.4 MB (67.36%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -1,35 +1,58 @@
+// class Solution {
+// public:
+//     bool solve(int i, int j, string s)
+//     {
+//         string temp;
+//         for(int k=i; k<=j; k++)
+//         {
+//             temp+=s[k];
+//         }
+//         unordered_map<char,int> mpp;
+//         for(auto it : temp)
+//         {
+//             mpp[it]++;
+//         }
+//         for(auto it : mpp)
+//         {
+//             if(it.second>2) return false;
+//         }
+//         return true;
+//     }
+//     int maximumLengthSubstring(string s) {
+//         int n=s.length();
+//         int maxi=0;
+//         for(int i=0; i<n; i++)
+//         {
+//             for(int j=i; j<n; j++)
+//             {
+//                 if(solve(i,j,s))
+//                 {
+//                     maxi=max(maxi,j-i+1);
+//                 }
+//             }
+//         }
+//         return maxi;
+//     }
+// };
 class Solution {
 public:
-    bool solve(int i, int j, string s)
-    {
-        string temp;
-        for(int k=i; k<=j; k++)
-        {
-            temp+=s[k];
-        }
-        unordered_map<char,int> mpp;
-        for(auto it : temp)
-        {
-            mpp[it]++;
-        }
-        for(auto it : mpp)
-        {
-            if(it.second>2) return false;
-        }
-        return true;
-    }
     int maximumLengthSubstring(string s) {
+        // optimized version using sliding window technique
         int n=s.length();
+        int left=0;
         int maxi=0;
-        for(int i=0; i<n; i++)
+        unordered_map<char,int> mpp;
+        for(int right=0; right<n; right++)
         {
-            for(int j=i; j<n; j++)
+            mpp[s[right]]++;
+            // shrink window 
+            while(mpp[s[right]] > 2)
             {
-                if(solve(i,j,s))
-                {
-                    maxi=max(maxi,j-i+1);
-                }
+                mpp[s[left]]--;
+                left++;
             }
+            // expand window cause it is right condition
+            maxi=max(maxi,right-left+1);
         }
         return maxi;
     }
