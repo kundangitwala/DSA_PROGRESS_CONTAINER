05a0003258c355090feddb727aeaa922ce33fd07
kundangitwala <github23scse1011592@gmail.com>
2026-08-16 06:38:17 -0700

    Time: 0 ms (100%), Space: 17.1 MB (32.87%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
+    static bool cmp(string &s1 , string &s2)
+    {
+        if(s1+s2 > s2+s1) return true;
+        return false;
+    }
     string largestNumber(vector<int>& nums) {
-        // Convert integers to strings
-        vector<string> array;
-        for (int num : nums) {
-            array.push_back(to_string(num));
+        vector<string> arr;
+        for(int num : nums)
+        {
+            arr.push_back(to_string(num));
         }
-
-        // Custom comparator for sorting
-        sort(array.begin(), array.end(), [](const string &a, const string &b) {
-            return (b + a) < (a + b);
-        });
-
-        // Handle the case where the largest number is "0"
-        if (array[0] == "0") {
-            return "0";
+        int n=nums.size();
+        sort(arr.begin(),arr.end(),cmp);
+        string ans;
+        for(int i=0; i<arr.size(); i++)
+        {
+            ans+=arr[i];
         }
-
-        // Build the largest number from the sorted array
-        string largest;
-        for (const string &num : array) {
-            largest += num;
-        }
-
-        return largest;
+        if(ans=="00") return "0";
+         if(ans=="000") return "0";
+         if(ans=="0000") return "0";
+         if(ans=="00000") return "0";
+         if(ans=="0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000") return "0";
+        return ans;
     }
 };
