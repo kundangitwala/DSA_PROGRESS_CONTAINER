882905c28976fae77bf2d0abc885c4ab8dfb971d
kundangitwala <github23scse1011592@gmail.com>
2026-08-26 20:32:02 +0530

    Time: 55 ms (5.04%), Space: 38.4 MB (5.79%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,67 +1,65 @@
 class Solution {
 public:
-
-    bool solve(int i, int j, string s, int k)
+    bool solve(int i , int j , string s , int p)
     {
-        int cnt = 0;
-
-        for(int x = i; x <= j; x++)
+        int cnt=0;
+        for(int k=i; k<=j; k++)
         {
-            if(s[x] == '1')
+            if(s[k] == '1')
             {
                 cnt++;
             }
         }
+        if(cnt==p) return true;
+        return false;
 
-        return cnt == k;
+
     }
-
     string construct(int i, int j, string s)
     {
         string temp;
-
-        for(int x = i; x <= j; x++)
+        for(int k=i; k<=j; k++)
         {
-            temp += s[x];
+            temp+=s[k];
         }
-
         return temp;
     }
-
-    string shortestBeautifulSubstring(string s, int k)
-    {
-        string mini = "";
-        int n = s.length();
-
-        for(int i = 0; i < n; i++)
+    string shortestBeautifulSubstring(string s, int k) {
+        // STEP 1 FIND ALL THE SUBSTRING OF S
+        // STEP 2 CONSIDER ONLY THE SUBSTRING WHICH CONSIST NO OF 1 EQUAL TO K IN THAT SUBSTRING
+        // STEP 3 TAKE ONLY WHICH IS SMALLER IN TERMS OF LEXICOGRAPHICALLY 
+        // STEP 4 RETURN THAT SUBSTRING
+        int one=0;
+        for(int i=0; i<s.length(); i++)
         {
-            for(int j = i; j < n; j++)
+            if(s[i] == '1') one++;
+
+        }
+        if(one<k) return "";
+        string mini=string(100,'1');
+        int n=s.length();
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i; j<n; j++)
             {
-                if(solve(i, j, s, k))
+                if(solve(i,j,s,k))
                 {
-                    string curr = construct(i, j, s);
-
-                    // First beautiful substring
-                    if(mini == "")
+                    string lele=construct(i,j,s);
+                     // First priority = shortest
+                    if(lele.length() < mini.length())
                     {
-                        mini = curr;
+                        mini = lele;
                     }
 
-                    // Shorter substring
-                    else if(curr.length() < mini.length())
+                    // Second priority = lexicographically smaller
+                    else if(lele.length() == mini.length())
                     {
-                        mini = curr;
+                        mini = min(mini, lele);
                     }
 
-                    // Same length -> lexicographically smaller
-                    else if(curr.length() == mini.length())
-                    {
-                        mini = min(mini, curr);
-                    }
                 }
             }
         }
-
         return mini;
     }
 };
